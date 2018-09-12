cf041f49b7f3fd87cff925a4d7ccdbef0205a523
iris-yue-wang <irisluv@gmail.com>
2018-09-12 20:42:22 +0100

    add: spring boot basic

---
+++ src/main/java/com/zuhlke/ihub/mart/controllers/ReportController.java
@@ -0,0 +1,13 @@
+package com.zuhlke.ihub.mart.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+
+    @PostMapping("/fnv-api/V1/holdings")
+    public void getHoldingsData() {
+
+    }
+}
